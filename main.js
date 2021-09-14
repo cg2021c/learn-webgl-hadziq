d618b1d3f0ad647ef9183c0f07ea5750a0d2bbba
Hadziq Fabroyir <hadziq@gmail.com>
2021-09-14 09:50:50 +0700

    Draw a full rectangle using gl.TRIANGLES

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ function main() {
          0.5, -0.5, 0.0, 1.0, 0.0,    // Point B
         -0.5, -0.5, 1.0, 0.0, 0.0,    // Point A
         -0.5,  0.5, 0.0, 0.0, 1.0,    // Point C
-         0.5,  0.5, 1.0, 1.0, 1.0     // Point D
+         0.5,  0.5, 1.0, 1.0, 1.0,    // Point D
+         0.5, -0.5, 0.0, 1.0, 0.0,    // Point B
+        -0.5,  0.5, 0.0, 0.0, 1.0     // Point C
     ];
 
     // Create a linked-list for storing the vertices data
@@ -91,8 +93,8 @@ function main() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     
-    var primitive = gl.TRIANGLE_FAN;
+    var primitive = gl.TRIANGLES;
     var offset = 0;
-    var nVertex = 4;
+    var nVertex = 6;
     gl.drawArrays(primitive, offset, nVertex);
 }
